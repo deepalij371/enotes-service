597863e420ca2c15b27ddbe6361898e7d92fd610
deepali <deepalij371@gmail.com>
2025-05-16 23:35:01 +0530

    save get category

---
+++ src/main/java/com/demo/entity/BaseModel.java
@@ -0,0 +1,26 @@
+package com.demo.entity;
+
+import java.util.Date;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@MappedSuperclass
+public class BaseModel {
+	
+    private Boolean isActive;
+	
+	private Boolean isDeleted;
+	
+	private Integer createdBy;
+	
+	private Date createdOn;
+	
+	private Integer updatedBy;
+	
+	private Date updatedOn;
+
+
+}
